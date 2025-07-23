9672888e72b83aa52377e086657442b5fc0c76fc
Evelyn Wu <Van_Wu1@outlook.com>
2025-07-23 16:32:57 +0100

    .

--- scr/py/Slope_EA.r
+++ scr/py/Slope_EA.r
@@ -1,3 +1,4 @@
+#不行啊不行...
 # 如果没装 remotes 包，先装它
 install.packages("remotes")
 remotes::install_github("h-a-graham/EAlidaR")
@@ -10,7 +11,7 @@ library(sf)
 # 你可以直接用 EA_BNG("Greater London") 来获取边界（EPSG:27700）
 bbox <- EA_BNG("Greater London")  # 返回 sf polygon
 
-# 检查能下载哪些 tile
+# 检查能下载哪些 tile???
 tiles <- available_tiles(bbox = bbox, dataset = "composite", product = "DTM", resolution = 1)
 
 
@@ -18,7 +19,7 @@ tiles <- available_tiles(bbox = bbox, dataset = "composite", product = "DTM", re
 # 下载并保存 DEM（默认保存为 GeoTIFF）
 download_tiles(
   tiles = tiles,
-  folder = "lidar_london",     # 本地保存文件夹
+  folder = "lidar_london",
   dataset = "composite",
   product = "DTM",
   resolution = 1,
